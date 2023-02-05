b1da64e3d91cf6010847bec32fc1935561b40188
Leandro Nazar <ljnazar@gmail.com>
2023-02-05 14:20:20 -0300

    Add_dotenv

--- 1ra_Pre_Entrega/src/app.js
+++ 1ra_Pre_Entrega/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
@@ -11,5 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-const server = app.listen(8080, ()=> console.log('Server running on port 8080'));
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, ()=> console.log('Server running on port 8080'));
 server.on('error', error => console.log(error));
