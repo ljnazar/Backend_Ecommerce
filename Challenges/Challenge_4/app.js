a3fe017a0d8c59a30ee192e50fc93299565ffe93
Leandro Nazar <ljnazar@gmail.com>
2023-02-06 20:17:50 -0300

    Add-&-Fix_Socket-io_Functionalities

--- Challenges/Challenge_4/app.js
+++ Challenges/Challenge_4/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const httpServer = http.createServer(app);
 const { Server } = require('socket.io');
+const io = new Server(httpServer);
+const env = require('dotenv')
 const handlebars = require('express-handlebars');
-require('dotenv').config();
 
+env.config();
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -13,33 +17,64 @@ app.engine('handlebars', handlebars.engine());
 app.set('views', 'views');
 app.set('view engine', 'handlebars');
 
+let dataProducts = [];
+
 app.get('/', (req, res) => {
-    res.render('index', {});
+    res.render('home', { data: dataProducts });
 });
 
-app.post('/nuevoMensaje', (req, res) => {
-    let { mensaje } = req.body;
-    dataCompleta.push(mensaje);
-    io.sockets.emit('messages', dataCompleta);
-    res.send('Mensaje enviado');
-})
+app.get('/realTimeProducts', (req, res) => {
+    res.render('realTimeProducts');
+});
 
-let dataCompleta = [];
+/* To develop with endpoints  */
 
-const PORT = process.env.PORT;
-
-const httpServer = app.listen(PORT, () => console.log('Server running on port 8080'));
-httpServer.on('error', error => console.log(error));
-
-const io = new Server(httpServer);
+// app.post('/addProduct', (req, res) => {
+//     let product = req.body;
+//     let productString = JSON.stringify(product);
+//     dataProducts.push(productString);
+//     io.sockets.emit('viewProducts', dataProducts);
+//     res.send('Producto agregado');
+// });
+// app.delete('/deleteProduct/:idp', (req, res) => {
+//     let { idp } = req.params;
+//     let dataProductsObj = [];
+//     dataProducts.map( product => {
+//         let auxObj = JSON.parse(product);
+//         dataProductsObj.push(auxObj);
+//     });
+//     dataProductsObj = dataProductsObj.filter(product => product.id != idp) || null;
+//     dataProducts = [];
+//     dataProductsObj.map( product => {
+//         let auxString = JSON.stringify(product);
+//         dataProducts.push(auxString);
+//     });
+//     io.sockets.emit('viewProducts', dataProducts);
+//     res.send('Producto Eliminado');
+// });
 
 io.on('connection', socket => {
-    console.log('Nuevo cliente conectado');
 
-    io.sockets.emit('messages', dataCompleta);
+    console.log('a user connected');
 
-    socket.on('message', data => {
-        dataCompleta.push(data);
-        io.sockets.emit('messages', dataCompleta); 
-    })
-})
+    socket.emit('viewProducts', dataProducts);
+
+    socket.on('addProduct', product => {
+        dataProducts.push(product);
+        io.sockets.emit('viewProducts', dataProducts);
+    });
+
+    socket.on('deleteProduct', product => {
+        dataProducts = dataProducts.filter(element => element != product) || null;
+        io.sockets.emit('viewProducts', dataProducts);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    
+});
+
+const PORT = process.env.PORT || 8080;
+
+httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- Challenges/Challenge_4/public/js/index.js
+++ Challenges/Challenge_4/public/js/index.js
@@ -1,5 +1,18 @@
 const socket = io();
 
-socket.on('messages', data => {
+const inputAdd = document.getElementById('inputAdd');
+const btnAdd = document.getElementById('btnAdd');
+const inputDelete = document.getElementById('inputDelete');
+const btnDelete = document.getElementById('btnDelete');
+
+btnAdd.addEventListener('click', () => {
+    socket.emit('addProduct', inputAdd.value);
+});
+
+btnDelete.addEventListener('click', () => {
+    socket.emit('deleteProduct', inputDelete.value);
+});
+
+socket.on('viewProducts', data => {
     document.querySelector('p').innerText = data;
 })
